46e18b14551c2167b64524187952d9e57313d0af
chenyutung <90234852+chenyutung@users.noreply.github.com>
2023-03-23 20:32:50 -0400

    a

--- pan.js
+++ pan.js
@@ -37,3 +37,14 @@ const animate = function () {
 }
 
 animate()
+
+$ = function(id) {
+    return document.getElementById(id);
+  }
+  
+  var show = function(id) {
+      $(id).style.display ='block';
+  }
+  var hide = function(id) {
+      $(id).style.display ='none';
+  }
